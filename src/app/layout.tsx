55d3ba6145341c7a9cae51fb50fe987a815b46f3
Maitann <hiagomaitan@gmail.com>
2023-08-09 04:29:15 -0300

    FEAT: new background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="bg-zinc-900 text-zinc-50">{children}</body>
+    <html lang="pt">
+      <body className="bg-black text-zinc-50">{children}</body>
     </html>
   )
 }
